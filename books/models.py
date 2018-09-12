2c54219dc5364e4747b813a26b80c403f1044384
Tony <2296458423@qq.com>
2018-09-12 21:59:04 +0800

    DRF框架基本结构搭建

--- books/models.py
+++ books/models.py
@@ -3,7 +3,36 @@ from django.db import models
 # Create your models here.
 
 class BookInfo(models.Model):
-    pass
+    btitle = models.CharField(max_length=20, verbose_name='书名')
+    bpub_date = models.DateField(verbose_name='发布日期')
+    bread = models.IntegerField(default=0, verbose_name='阅读量')
+    bcomment = models.IntegerField(default=0, verbose_name='评论量')
+    bimage = models.ImageField(null=True, blank=True, upload_to='books/', verbose_name='图片')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'tb_books'
+        verbose_name = '图书'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.btitle
 
 class HeroInfo(models.Model):
-    pass
+    GENDER_CHOICES = (
+        (0, 'female'),
+        (1, 'male')
+    )
+    hname = models.CharField(max_length=20, verbose_name='姓名')
+    hgender = models.SmallIntegerField(choices=GENDER_CHOICES, default=0, verbose_name='性别')
+    hcomment = models.CharField(max_length=200, verbose_name='人物描述')
+    hbook = models.ForeignKey(BookInfo, on_delete=models.CASCADE, verbose_name='图书')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'tb_heros'
+        verbose_name = '英雄'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.hname
--- books/serializers.py
+++ books/serializers.py
@@ -1,4 +1,24 @@
 from rest_framework import serializers
+from books.models import BookInfo, HeroInfo
 
 class BookInfoSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    btitle = serializers.CharField(max_length=20, label='书名')
+    bpub_date = serializers.DateField(label='发布日期')
+    bread = serializers.IntegerField(default=0, label='阅读量')
+    bcomment = serializers.IntegerField(default=0, label='评论量')
+    bimage = serializers.ImageField(allow_null=True, label='图片')
+    is_delete = serializers.BooleanField(default=False, label='逻辑删除')
+    heroinfo_set = serializers.StringRelatedField(read_only=True, many=True)
+
+
+class HeroInfoSerializer(serializers.Serializer):
+    GENDER_CHOICES = (
+        (0, 'female'),
+        (1, 'male')
+    )
+    hname = serializers.CharField(max_length=20, label='姓名')
+    hgender = serializers.ChoiceField(choices=GENDER_CHOICES, default=0, label='性别')
+    hcomment = serializers.CharField(max_length=200, label='人物描述')
+    is_delete = serializers.BooleanField(default=False, label='逻辑删除')
+    hbook = serializers.PrimaryKeyRelatedField(queryset=BookInfo.objects.all(), label='图书', many=True)
