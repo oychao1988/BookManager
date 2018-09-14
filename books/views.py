f16ecfb0be766aa71532e0561b5a4aafe01c9cf3
Tony <2296458423@qq.com>
2018-09-14 09:42:28 +0800

    视图继承五个扩展类

--- books/views.py
+++ books/views.py
@@ -1,34 +1,10 @@
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 
 from books.models import BookInfo
 from books.serializers import BookInfoSerializer
 
-class BooksInfoView(GenericViewSet):
+class BooksInfoView(GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
     queryset = BookInfo.objects.all()
     serializer_class = BookInfoSerializer
-
-    def list(self, requset):
-        serializer = self.get_serializer(instance=self.get_queryset(), many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk):
-        serializer = self.get_serializer(instance=self.get_object())
-        return Response(serializer.data)
-
-    def update(self, request, pk):
-        serializer = self.get_serializer(self.get_object(), data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def destroy(self, request, pk):
-        book = self.get_object()
-        book.delete()
-        return Response(status=204)
