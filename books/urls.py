4e254204b21862bbedf525438b96052a3ed70ce1
Tony <2296458423@qq.com>
2018-09-14 10:09:40 +0800

    类视图继承ModelViewSet

--- books/urls.py
+++ books/urls.py
@@ -1,14 +1,8 @@
-from django.conf.urls import url
-from books.views import BooksInfoView
-
-urlpatterns = [
-    url(r'^books/$', BooksInfoView.as_view({'get': 'list', 'post': 'create'})),
-    url(r'^books/(?P<pk>\d+)/$', BooksInfoView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}))
-]
-
 from rest_framework.routers import DefaultRouter
-# from books.views import BookInfoViewSet
-# myRouter = DefaultRouter()
-# myRouter.register(r'books', BookInfoViewSet, base_name='books')
-#
-# urlpatterns += myRouter.urls
+from books.views import BookInfoViewSet
+
+urlpatterns = []
+myRouter = DefaultRouter()
+myRouter.register(r'books', BookInfoViewSet, base_name='books')
+urlpatterns += myRouter.urls
+
--- books/views.py
+++ books/views.py
@@ -1,10 +1,8 @@
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from books.models import BookInfo
 from books.serializers import BookInfoSerializer
 
-class BooksInfoView(GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+class BookInfoViewSet(ModelViewSet):
     queryset = BookInfo.objects.all()
     serializer_class = BookInfoSerializer
